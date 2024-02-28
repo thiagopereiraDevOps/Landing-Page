8a7581c30a9a9e46fd542e59bf2bdf554dc56c1e
Thiago Pereira Da Silva <thiago.silva@venturus.org.br>
2024-02-28 10:51:45 -0300

    Adicionar de Landing-Page

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [HomeComponent,
+    NgOptimizedImage
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
